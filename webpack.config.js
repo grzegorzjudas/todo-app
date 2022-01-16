1636a7b500bb3c419384a721b831197706317d2f
Grzegorz Judas <grzegorz.judas@gmail.com>
2022-01-16 22:43:38 +0100

    Basic react app

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, './dist/'),
+        filename: 'index.js'
+    },
+    resolve: {
+        extensions: [ '.js', '.ts', '.tsx' ]
+    },
+    mode: 'production',
+    devServer: {
+        port: 8080
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts|tsx)$/,
+                exclude: [ /node_modules/ ],
+                use: [
+                    { loader: 'ts-loader' }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'head',
+            scriptLoading: 'defer'
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: './src/style.css', to: './' }
+            ]
+        })
+    ]
+};
